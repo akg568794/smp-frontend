95dcd255898676bf4c99c2fba1f7790700c05abe
abhay gupta <akg568794@gmail.com>
2024-07-28 14:18:32 +0530

    made some changes for deployment

--- src/component/MusicPlayer.jsx
+++ src/component/MusicPlayer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://smp-backend.onrender.com');
 
 const MusicPlayer = () => {
   const audioRef = useRef(null);
